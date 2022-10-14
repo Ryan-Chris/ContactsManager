2b18bec96742621378eaa5b5acce237123de7e5d
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-14 14:25:23 -0500

    updated

--- ContactsApplication/ContactsApp.java
+++ ContactsApplication/ContactsApp.java
@@ -4,28 +4,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class ContactsApp {
 
     public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        Path currentDir = Paths.get("ContactsApplication/contacts.txt");
 
-        String directory = "ContactsApplication";
-        String filename = "contacts.txt";
 
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
+//        System.out.println("Name: ");
+//        String userName = scan.nextLine();
+//        List<String> contactName = Arrays.asList(userName);
+//        System.out.println(contactName);
+//        System.out.println("Number: ");
+//        String userNumber = scan.nextLine();
+//        List<String> contactNumber = Arrays.asList(userNumber);
+//        System.out.println(contactNumber);
 
-        if (Files.notExists(dataDirectory)) {
-            Files.createDirectories(dataDirectory);
+
+//        Files.write(currentDir, contactName);
+//        Files.write(currentDir, contactNumber, StandardOpenOption.APPEND);
+
+        List<String> contactList = Files.readAllLines(currentDir);
+        for(int i = 0; i < contactList.size(); i++) {
+            System.out.println((i + 1) + ": " + contactList.get(i));
         }
 
-        if (! Files.exists(dataFile)) {
-            Files.createFile(dataFile);
+    }
 
-
-
-        }
-
-}}
+}
 
 
---
+++ ContactsApplication/contacts.java
@@ -0,0 +1,5 @@
+package ContactsApplication;
+
+public class contacts {
+
+}
